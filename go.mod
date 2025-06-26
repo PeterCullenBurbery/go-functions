9042db286e7a084f257c7c45a81058eb07b96a25
Peter Cullen Burbery <peter.cullen.burbery@gmail.com>
2025-06-26 18:41:52 -0400

    hello

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/PeterCullenBurbery/go-functions
+
+go 1.24.4
---
+++ hello.go
@@ -0,0 +1,8 @@
+// hello.go
+package gofunctions
+
+import "fmt"
+
+func SayHello(name string) {
+	fmt.Printf("Hello, %s!\n", name)
+}
